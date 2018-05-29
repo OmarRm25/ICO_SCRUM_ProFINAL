5189e59280ed990a5ba07ea654d199c445ded890
HMikado <victorf9523@gmail.com>
2018-05-28 21:40:09 -0500

    Se agrego pantallamenu y se renombro a jabon judio

---
+++ JabonJudio/src/jabonjudio/JabonJudio.java
@@ -0,0 +1,14 @@
+ 
+package jabonjudio;
+
+ 
+public class JabonJudio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
